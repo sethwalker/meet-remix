698309523dfb7b8d8ef061f0bae1ebe34983ec44
Seth Walker <seth.h.walker@gmail.com>
2022-11-06 22:41:01 -0500

    room create

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,20 @@
+import { Form } from "@remix-run/react";
+import { redirect } from "@remix-run/node";
+
+export async function action({ request }) {
+  const body = await request.formData();
+  const room = body.get("roomName");
+  return redirect(`/${room}`);
+}
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Meet with </h1>
+      <Form method="post">
+        <input type="text" name="roomName" />
+        <input type="submit" />
+      </Form>
     </div>
   );
 }
